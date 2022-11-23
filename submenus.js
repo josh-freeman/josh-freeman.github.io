e84aa10808b25fc33f2172aff761efb9d63c1440
jfreeman <joshua.freeman@epfl.ch>
2022-11-23 14:49:18 +0000

    submenus except html

---
+++ submenus.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $('subsubmenu').hide();
+    
+    $('submenu').click(function(event) {
+      event.stopPropagation();
+      $('> subsubmenu', this).slideToggle(300);
+  
+    });
+  });
+  

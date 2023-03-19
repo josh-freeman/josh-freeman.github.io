c96157275cf57e28dab284fb34786e203769a423
jfreeman <joshua.freeman@epfl.ch>
2023-03-19 09:49:19 +0100

    ids and stuff

--- cards.js
+++ cards.js
@@ -1,22 +1,25 @@
-const card = document.querySelector('.card');
-const cardInner = document.querySelector('.card-inner');
-const readMoreLink = document.querySelector('.read-more');
-const readLessLink = document.querySelector('.read-less');
+const cards = document.querySelectorAll('.card');
+const cardInners = document.querySelectorAll('.card-inner');
+const readMoreLinks = document.querySelectorAll('.read-more');
+const readLessLinks = document.querySelectorAll('.read-less');
 
-readMoreLink.addEventListener('click', function(e) {
-  e.preventDefault();
-  cardInner.style.transform = 'rotateY(180deg)';
-  readMoreLink.style.display = 'none';
-  readLessLink.style.display = 'block';
+readMoreLinks.forEach((readMoreLink, index) => {
+  readMoreLink.addEventListener('click', function(e) {
+    e.preventDefault();
+    cardInners[index].style.transform = 'rotateY(180deg)';
+    readMoreLinks[index].style.display = 'none';
+    readLessLinks[index].style.display = 'block';
+  });
 });
 
-readLessLink.addEventListener('click', function(e) {
-  e.preventDefault();
-  cardInner.style.transform = 'rotateY(0deg)';
-  readMoreLink.style.display = 'block';
-  readLessLink.style.display = 'none';
+readLessLinks.forEach((readLessLink, index) => {
+  readLessLink.addEventListener('click', function(e) {
+    e.preventDefault();
+    cardInners[index].style.transform = 'rotateY(0deg)';
+    readMoreLinks[index].style.display = 'block';
+    readLessLinks[index].style.display = 'none';
+  });
 });
-
 
 
     

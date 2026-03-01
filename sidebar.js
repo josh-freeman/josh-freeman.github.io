42c210b38701ca9740c14e8503a1bd1cdae5ab07
josh-freeman <freemanjosh1000@gmail.com>
2026-03-01 15:16:48 -0500

    Fix profile link in sidebar to include user ID

--- sidebar.js
+++ sidebar.js
@@ -82,7 +82,7 @@ function loadNavbarDiv() {
             accountWidget = `
                 <div class="sidebar-account">
                     <div class="sidebar-account-row">
-                        <a href="/profile.html" class="sidebar-account-info">
+                        <a href="/profile.html?id=${user.id}" class="sidebar-account-info">
                             <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"/><circle cx="12" cy="7" r="4"/></svg>
                             <span>${userName}</span>
                         </a>
